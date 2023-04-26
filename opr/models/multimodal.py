5c67e7a3b19ba426de17ea01d349580aa8ba5a68
ms.Ana <ms.urmanita@gmail.com>
2023-04-26 08:53:58 +0300

    add config to multimodal

--- opr/models/multimodal.py
+++ opr/models/multimodal.py
@@ -4,12 +4,12 @@ from typing import Optional, Union
 
 import torch
 from hydra.utils import instantiate
-from omegaconf import DictConfig
+from omegaconf import DictConfig, OmegaConf
 
 from .base_models import ComposedModel
 
 
-def minkloc_multimodal(weights: Optional[Union[str, Path]] = None) -> ComposedModel:
+def minkloc_multimodal(model_config: DictConfig, weights: Optional[Union[str, Path]] = None) -> ComposedModel:
     """Default MinkLoc++ model configuration.
 
     Paper: https://arxiv.org/abs/2104.05327
@@ -27,36 +27,38 @@ def minkloc_multimodal(weights: Optional[Union[str, Path]] = None) -> ComposedMo
     Returns:
         ComposedModel: MinkLoc++ model with default configuration from the original paper.
     """
-    model_config = DictConfig(
-        {
-            "_target_": "opr.models.base_models.ComposedModel",
-            "image_module": {
-                "_target_": "opr.models.base_models.ImageModule",
-                "backbone": {
-                    "_target_": "opr.models.resnet.ResNet18FPNExtractor",
-                    "lateral_dim": 128,
-                    "fh_num_bottom_up": 4,
-                    "fh_num_top_down": 0,
-                    "pretrained": True,
-                },
-                "head": {"_target_": "opr.models.layers.gem.GeM", "p": 3, "eps": 1e-06},
-            },
-            "cloud_module": {
-                "_target_": "opr.models.base_models.CloudModule",
-                "backbone": {
-                    "_target_": "opr.models.minkloc.MinkResNetFPNExtractor",
-                    "out_channels": 128,
-                    "num_top_down": 1,
-                    "conv0_kernel_size": 5,
-                    "block": "ECABasicBlock",
-                    "layers": [1, 1, 1],
-                    "planes": [32, 64, 64],
-                },
-                "head": {"_target_": "opr.models.layers.gem.MinkGeM", "p": 3, "eps": 1e-06},
-            },
-            "fusion_module": {"_target_": "opr.models.fusion.Concat"},
-        }
-    )
+    # model_config = DictConfig(
+    #     {
+    #         "_target_": "opr.models.base_models.ComposedModel",
+    #         "image_module": {
+    #             "_target_": "opr.models.base_models.ImageModule",
+    #             "backbone": {
+    #                 "_target_": "opr.models.resnet.ResNet18FPNExtractor",
+    #                 "lateral_dim": 128,
+    #                 "fh_num_bottom_up": 4,
+    #                 "fh_num_top_down": 0,
+    #                 "pretrained": True,
+    #             },
+    #             "head": {"_target_": "opr.models.layers.gem.GeM", "p": 3, "eps": 1e-06},
+    #         },
+    #         "cloud_module": {
+    #             "_target_":-y "opr.models.base_models.CloudModule",
+    #             "backbone": {
+    #                 "_target_": "opr.models.minkloc.MinkResNetFPNExtractor",
+    #                 "out_channels": 128,
+    #                 "num_top_down": 1,
+    #                 "conv0_kernel_size": 5,
+    #                 "block": "ECABasicBlock",
+    #                 "layers": [1, 1, 1],
+    #                 "planes": [32, 64, 64],
+    #             },
+    #             "head": {"_target_": "opr.models.layers.gem.MinkGeM", "p": 3, "eps": 1e-06},
+    #         },
+    #         "fusion_module": {"_target_": "opr.models.fusion.Concat"},
+    #     }
+    # )
+    
+    model_config = OmegaConf.load(model_config)
     model = instantiate(model_config)
     if weights is not None:
         if not Path(weights).exists():
