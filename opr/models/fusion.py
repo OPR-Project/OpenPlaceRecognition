a642e69332fa43ce64d32f2d3aa6048e0afbfbe4
ms.Ana <ms.urmanita@gmail.com>
2023-04-28 07:05:30 +0300

    change fusion behaviour to concat all modalities

--- opr/models/fusion.py
+++ opr/models/fusion.py
@@ -9,14 +9,14 @@ from opr.models.base_models import FusionModule
 
 
 class Concat(FusionModule):
-    """Concatenation module for 'image' and 'cloud' modalities."""
+    """Concatenation module for all modalities."""
 
     def __init__(self) -> None:
-        """Concatenation module for 'image' and 'cloud' modalities."""
+        """Concatenation module for all modalities."""
         super().__init__()
 
     def forward(self, data: Dict[str, Union[Tensor, ME.SparseTensor]]) -> Tensor:  # noqa: D102
-        assert "image" in data
-        assert "cloud" in data
-        fusion_global_descriptor = torch.concat([data["image"], data["cloud"]], dim=1)
+        # assert "image" in data
+        # assert "cloud" in data
+        fusion_global_descriptor = torch.concat(list(data.values()), dim=1)
         return fusion_global_descriptor
