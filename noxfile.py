00cbfae8fb75d98fdd91ec1b27292a92c166f655
Alexander Melekhin <amelekhin96@gmail.com>
2023-07-11 12:01:37 +0000

    chore: add linting session to nox

--- noxfile.py
+++ noxfile.py
@@ -2,6 +2,8 @@
 import nox
 from nox.sessions import Session
 
+nox.options.sessions = "tests", "lint"
+
 PYTHON_VERSIONS = ("3.8", "3.9", "3.10")
 PYTORCH_VERSIONS = ("1.12.1", "1.13.1", "2.0.1")
 TORCHVISION_VERSIONS_DICT = {
@@ -10,6 +12,7 @@ TORCHVISION_VERSIONS_DICT = {
     "2.0.1": "0.15.2",
 }
 
+locations = "src", "tests", "noxfile.py"
 package = "opr"
 
 
@@ -46,3 +49,15 @@ def tests(session: Session, pytorch: str) -> None:
     session.install(".")
     session.install("-r", "requirements-dev.txt")
     session.run("pytest", *args)
+
+
+@nox.session
+@nox.parametrize("python", PYTHON_VERSIONS)
+def lint(session: Session) -> None:
+    """Lint using flake8."""
+    args = session.posargs or locations
+    install_cpu_torch(session)
+    install_minkowskiengine(session)
+    session.install(".")
+    session.install("-r", "requirements-dev.txt")
+    session.run("flake8", *args)
