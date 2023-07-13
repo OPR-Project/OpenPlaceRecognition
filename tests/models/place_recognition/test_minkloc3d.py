85dae6cd37258ffdd10c4c89b9b5e6acc1eb1f68
Alexander Melekhin <amelekhin96@gmail.com>
2023-07-13 13:17:59 +0000

    test: mark test_minkloc3d_instantiate as end-to-end test

--- tests/models/place_recognition/test_minkloc3d.py
+++ tests/models/place_recognition/test_minkloc3d.py
@@ -1,10 +1,12 @@
 """Test cases for opr.models.place_recognition.minkloc3d module."""
+import pytest
 from hydra.utils import instantiate
 
 from opr.models.place_recognition.minkloc3d import MinkLoc3D
 from tests.utils import load_config
 
 
+@pytest.mark.e2e
 def test_minkloc3d_instantiate() -> None:
     """Should instantiate MinkLoc3D object."""
     config = load_config("configs/model/place_recognition/minkloc3d.yaml")
