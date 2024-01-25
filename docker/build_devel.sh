96424baa7dd9a9ed97e2ca8e48edece7dc605c3c
Alexander Melekhin <amelekhin96@gmail.com>
2024-01-25 17:48:50 +0300

    chore: docker image update

--- docker/build_devel.sh
+++ docker/build_devel.sh
@@ -9,9 +9,9 @@ if [ $ARCH != "x86_64" ]; then
     exit 1
 fi
 
-if command -v nvcc &> /dev/null; then
+if command -v nvidia-smi &> /dev/null; then
     echo "Detected ${orange}CUDA${reset_color} hardware"
-    DOCKERFILE=Dockerfile.cuda
+    DOCKERFILE=Dockerfile.devel
     DEVICE=cuda
 else
     echo "${orange}CPU-only${reset_color} build is not supported yet"
@@ -23,8 +23,9 @@ echo "Building for ${orange}${ARCH}${reset_color} with ${orange}${DEVICE}${reset
 PROJECT_ROOT_DIR=$(cd ./"`dirname $0`"/.. || exit; pwd)
 
 docker build $PROJECT_ROOT_DIR \
-    -f $PROJECT_ROOT_DIR/docker/$DOCKERFILE \
+    --build-arg MAX_JOBS=4 \
     --build-arg UID=$(id -u) \
     --build-arg GID=$(id -g) \
-    -t open-place-recognition:$DEVICE-$USER \
+    -f $PROJECT_ROOT_DIR/docker/$DOCKERFILE \
+    -t open-place-recognition:devel \
     --network=host
