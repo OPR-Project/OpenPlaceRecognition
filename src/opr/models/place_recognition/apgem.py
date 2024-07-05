a819e19ff25c853c9e5f40df6b3197d9acfd0ca7
Alexander Melekhin <amelekhin96@gmail.com>
2024-07-05 22:22:52 +0300

    feat: APGeM implementation

---
+++ src/opr/models/place_recognition/apgem.py
@@ -0,0 +1,31 @@
+from torch.nn.modules import Module
+from opr.modules.feature_extractors import ResNet50FPNFeatureExtractor
+from opr.modules import GeM
+
+from .base import ImageModel
+
+
+class APGeMImageModel(ImageModel):
+    """APGeM: 'Learning with Average Precision: Training Image Retrieval with a Listwise Loss'.
+
+    Paper: https://arxiv.org/abs/1906.07589
+    """
+
+    def __init__(self, backbone: str = "resnet50") -> None:
+        """Initialize APGeM Image Model.
+
+        Args:
+            backbone (str): Backbone architecture. Defaults to "resnet50".
+
+        Raises:
+            NotImplementedError: If given backbone is unknown.
+        """
+        if backbone == "resnet50":
+            backbone = ResNet50FPNFeatureExtractor()
+        else:
+            raise NotImplementedError(f"Backbone {backbone} is not supported.")
+        head = GeM()
+        super().__init__(
+            backbone=backbone,
+            head=head,
+        )
