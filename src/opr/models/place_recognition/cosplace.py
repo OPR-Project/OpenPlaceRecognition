e61b9285f6cccf2adced7b61de6631b15605ce8d
Alexander Melekhin <amelekhin96@gmail.com>
2024-07-23 13:30:26 +0000

    feat: more backbones for cosplace

--- src/opr/models/place_recognition/cosplace.py
+++ src/opr/models/place_recognition/cosplace.py
@@ -1,6 +1,12 @@
 """Implementation of CosPlace model."""
+from typing import Literal
+
 from opr.modules.cosplace import CosPlace
-from opr.modules.feature_extractors import ResNet50FPNFeatureExtractor
+from opr.modules.feature_extractors import (
+    ResNet18FPNFeatureExtractor,
+    ResNet50FPNFeatureExtractor,
+    VGG16FeatureExtractor,
+)
 
 from .base import ImageModel
 
@@ -11,7 +17,7 @@ class CosPlaceModel(ImageModel):
     Paper: https://arxiv.org/abs/2204.02287
     """
 
-    def __init__(self, backbone: str = "resnet50") -> None:
+    def __init__(self, backbone: Literal["resnet18", "resnet50", "vgg16"] = "resnet50") -> None:
         """Initialize CosPlace Image Model.
 
         Args:
@@ -20,8 +26,12 @@ class CosPlaceModel(ImageModel):
         Raises:
             NotImplementedError: If given backbone is unknown.
         """
-        if backbone == "resnet50":
+        if backbone == "resnet18":
+            backbone = ResNet18FPNFeatureExtractor()
+        elif backbone == "resnet50":
             backbone = ResNet50FPNFeatureExtractor()
+        elif backbone == "vgg16":
+            backbone = VGG16FeatureExtractor()
         else:
             raise NotImplementedError(f"Backbone {backbone} is not supported.")
         head = CosPlace()
