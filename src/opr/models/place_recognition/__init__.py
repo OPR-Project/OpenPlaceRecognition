07ac246296859afbf8e1473d810584af0bb8b1d6
Alexander Melekhin <amelekhin96@gmail.com>
2025-01-22 16:56:37 +0000

    feat: implementation of PointMamba for PlaceRecognition

--- src/opr/models/place_recognition/__init__.py
+++ src/opr/models/place_recognition/__init__.py
@@ -5,6 +5,7 @@ from .minkloc import MinkLoc3D, MinkLoc3Dv2
 from .netvlad import NetVLADModel
 from .overlaptransformer import OverlapTransformer
 from .patchnetvlad import PatchNetVLAD
+from .pointmamba import PointMambaPR
 from .pointnetvlad import PointNetVLAD
 from .resnet import ResNet18, SemanticResNet18
 from .svtnet import SVTNet
