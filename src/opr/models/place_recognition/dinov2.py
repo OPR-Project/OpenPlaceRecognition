fd4653cf5e2c18e9b1c6703690c9619624fb3d7c
Alexander Melekhin <amelekhin96@gmail.com>
2025-03-11 17:00:14 +0300

    feat: add None options to Dinov2

--- src/opr/models/place_recognition/dinov2.py
+++ src/opr/models/place_recognition/dinov2.py
@@ -1,4 +1,6 @@
 import torch
+from torch import nn
+
 from .base import ImageModel
 from opr.modules.feature_extractors.dinov2 import DINO_V2_MODELS, DINO_FACETS, BOQ_MODELS
 from opr.modules.gem import GlobalAvgPooling, GlobalMaxPooling
@@ -8,13 +10,13 @@ from opr.modules.feature_extractors.dinov2 import ViTBaseFeatureExtractor
 class DinoV2(ImageModel):
     def __init__(
             self,
-            dino_model: DINO_V2_MODELS, 
-            layer: int, 
-            facet: DINO_FACETS="token", 
-            use_cls=False, 
-            norm_descs=True, 
+            dino_model: DINO_V2_MODELS,
+            layer: int,
+            facet: DINO_FACETS="token",
+            use_cls=False,
+            norm_descs=True,
             device: str = "cpu",
-            pooling: str = "gap",
+            pooling: str | None = "gap",
             weights_path = None,
         ) -> None:
         feature_extractor = ViTBaseFeatureExtractor(
@@ -30,9 +32,11 @@ class DinoV2(ImageModel):
             pooling = GlobalAvgPooling()
         elif pooling == "gmp":
             pooling = GlobalMaxPooling()
+        elif pooling is None:
+            pooling = nn.Identity()
         else:
             raise NotImplementedError("Unknown pooling method: {}".format(pooling))
-        
+
         super().__init__(
             backbone=feature_extractor,
             head=pooling,
