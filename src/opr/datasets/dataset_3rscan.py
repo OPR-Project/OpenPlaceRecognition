3b9ab2628fd9419f54e3690482fcc70c42a0c97c
TimKo <timophey04@gmail.com>
2025-04-14 23:00:16 +0300

    Added depth images and pose transformation between different scans

--- src/opr/datasets/dataset_3rscan.py
+++ src/opr/datasets/dataset_3rscan.py
@@ -1,6 +1,6 @@
 """Custom 3RScan dataset implementations."""
 from pathlib import Path
-from typing import Dict, List, Literal, Optional, Tuple, Union
+from typing import Dict, List, Literal, Optional, Tuple, Union, Set
 import zipfile
 import cv2
 import json
@@ -20,31 +20,39 @@ from opr.datasets.augmentations import (
 class Dataset3RScan(Dataset):
     """3RScan dataset implementation.
     
-    Requires downloaded 3RScan repository, executed setup.sh script and downloaded scenes in data/3RScan directory."""
+    Requires downloaded 3RScan repository, executed setup.sh script and downloaded scenes in data/3RScan directory.
+    
+    There is no pose transformation between different scans for test subset."""
 
 
     scenes_folder: Path
-    subset: Literal["train", "val", "test"]
+    subset: Literal["train", "validation", "test"]
     frame_ids: List[Tuple[str, int]]
     image_transform: DefaultImageTransform
+    load_depth_images: bool
+    depth_images_resize: Tuple[int, int]
 
 
     def __init__(
         self,
         dataset_root: Union[Path, str],
-        subset: Literal["train", "val", "test"],
+        subset: Literal["train", "validation", "test"],
         image_transform = DefaultImageTransform(resize=(322, 322), train=False),
         min_scans_cnt: int = 2,
-        picture_limit_per_scan: int = 50
+        picture_limit_per_scan: int = 50,
+        load_depth_images: bool = False,
+        depth_images_resize: Tuple[int, int] = (322, 322)
     ) -> None:
         """3RScan dataset implementation.
         
         Args:
             dataset_root (Union[Path, str]): Root directory of dataset.
-            subset (Literal["train", "val", "test"]): Dataset subset to load.
+            subset (Literal["train", "validation", "test"]): Dataset subset to load.
             image_transform: Image transform to apply to images. Defaults to DefaultImageTransform.
             min_scans_cnt (int): Minimum number of scans in a scene, that are loaded. Defaults to 2.
-            picture_limit_per_scan (int): Number of pictures to load from each scan. Defaults to 10.
+            picture_limit_per_scan (int): Number of pictures to load from each scan. Defaults to 50.
+            load_depth_images (bool): Whether to load depth images. Depth in mm. Defaults to False.
+            depth_images_resize (Tuple[int, int]): Resize dimensions for depth images. Defaults to (322, 322).
 
         Raises:
             FileNotFoundError: If dataset_root doesn't exist.
@@ -57,6 +65,9 @@ class Dataset3RScan(Dataset):
 
         self.subset = subset
 
+        self.load_depth_images = load_depth_images
+        self.depth_images_resize = depth_images_resize
+
         scene_folder = dataset_root / "data/3RScan"
 
         # Loading from 3RScan.json all the scene ids and their repeated scans
@@ -64,10 +75,12 @@ class Dataset3RScan(Dataset):
             json_3rscan = f.read()
         json_3rscan = json.loads(json_3rscan)
 
-        scene_ids: List[str] = []
+        scene_ids: Set[str] = set()
         scene_references: Dict[str, str] = {}
         track_numbers: Dict[str, int] = {}
         room_numbers: Dict[str, int] = {}
+        tranformation_matrices: Dict[str, np.ndarray] = {}
+
         room_cnt = 0
         for room in json_3rscan:
             if room["type"] != subset:
@@ -77,16 +90,18 @@ class Dataset3RScan(Dataset):
                 continue
 
             scene_id = room["reference"]
-            scene_ids.append(scene_id)
+            scene_ids.add(scene_id)
             scene_references[scene_id] = scene_id
             track_numbers[scene_id] = 0
             room_numbers[scene_id] = room_cnt
             room_cnt += 1
             for i, scan in enumerate(room["scans"]):
                 scan_id = scan["reference"]
-                scene_ids.append(scan_id)
+                scene_ids.add(scan_id)
                 scene_references[scan_id] = scene_id
                 track_numbers[scan_id] = i + 1
+                if "transform" in scan:
+                    tranformation_matrices[scan_id] = np.array(scan["transform"]).reshape((4, 4))
         
         # Saving frames ids
         self.scenes_folder = scene_folder
@@ -105,7 +120,7 @@ class Dataset3RScan(Dataset):
 
         self.image_transform = image_transform
 
-        self.dataset_df = self._generate_dataframe(scene_references, track_numbers, room_numbers)
+        self.dataset_df = self._generate_dataframe(scene_references, track_numbers, room_numbers, tranformation_matrices)
 
 
     def __getitem__(self, idx: int) -> Dict[str, Union[Tensor, int]]:
@@ -127,6 +142,9 @@ class Dataset3RScan(Dataset):
         data["pose"] = self._pose_to_6dof(idx)
 
         data["image_front_cam"] = self._load_image(idx, transform=True)
+
+        if self.load_depth_images:
+            data["depth_image_front_cam"] = self._load_depth_image(idx)
 
         return data
 
@@ -188,8 +206,23 @@ class Dataset3RScan(Dataset):
         scene_id, frame_id = self.frame_ids[idx]
         scene_path = self.scenes_folder / scene_id
         return scene_path / f"sequence/frame-{frame_id:06d}.pose.txt"
-
     
+
+    def _get_frame_depth_image_path(self, idx: int) -> Path:
+        """ Gets the path to the frame depth image.
+
+            Args:
+                idx (int): Frame index.
+            
+            Returns:
+                Path: Path to the frame depth image.
+        """
+
+        scene_id, frame_id = self.frame_ids[idx]
+        scene_path = self.scenes_folder / scene_id
+        return scene_path / f"sequence/frame-{frame_id:06d}.depth.pgm"
+
+
     def _load_image(self, idx: int, transform: bool = True) -> Tensor:
         """Load an image from the dataset.
         Args:
@@ -205,10 +238,25 @@ class Dataset3RScan(Dataset):
         if transform:
             im = self.image_transform(im)
         return im
+
+
+    def _load_depth_image(self, idx: int) -> Tensor:
+        """Load a depth image from the dataset.
+
+        Args:
+            idx (int): Frame index.
+
+        Returns:
+            Tensor: Loaded depth image tensor.
+        """
+
+        im = cv2.imread(str(self._get_frame_depth_image_path(idx)), cv2.IMREAD_UNCHANGED)
+        im = cv2.resize(im, self.depth_images_resize, interpolation=cv2.INTER_NEAREST)
+        return im
     
 
     def _generate_dataframe(self, scene_references: Dict[str, str], track_numbers: Dict[str, int],
-                            room_numbers: Dict[str, int]) -> pd.DataFrame:
+                            room_numbers: Dict[str, int], transformation_matrices: Dict[str, np.ndarray]) -> pd.DataFrame:
         """Generate a DataFrame with poses for dataset_df.
 
         Args:
@@ -222,8 +270,13 @@ class Dataset3RScan(Dataset):
 
         data: List[Dict[str, int]] = []
         for idx in range(len(self.frame_ids)):
+            scene_id, frame_id = self.frame_ids[idx]
             with open(self._get_frame_pos_path(idx), "r") as f:
                 pose = np.array([float(x) for x in f.read().split()]).reshape((4, 4))
+                
+                if scene_id in transformation_matrices:
+                    pose = pose @ transformation_matrices[scene_id]
+
                 translation = pose[:3, 3]  # [Tx, Ty, Tz]
                 rotation_matrix = pose[:3, :3]
                 quaternion = R.from_matrix(rotation_matrix).as_quat()  # [Qx, Qy, Qz, Qw]
@@ -231,7 +284,7 @@ class Dataset3RScan(Dataset):
                 tx, ty, tz = translation
                 qx, qy, qz, qw = quaternion
 
-            track = track_numbers[self.frame_ids[idx][0]]
+            track = track_numbers[scene_id]
 
             data.append({
                 # Adding coordinates to tx so that all rooms are in different coordinates
