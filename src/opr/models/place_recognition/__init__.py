1b3cdae79ac41d04337501ef9ca36505a319920e
Alexander Melekhin <amelekhin96@gmail.com>
2025-07-02 03:18:15 +0300

    feat: add SequenceLateFusionModel

--- src/opr/models/place_recognition/__init__.py
+++ src/opr/models/place_recognition/__init__.py
@@ -10,4 +10,4 @@ from .pointmamba import PointMambaPR
 from .pointnetvlad import PointNetVLAD
 from .resnet import ResNet18, SemanticResNet18
 from .svtnet import SVTNet
-# git
+from .sequential import SequenceLateFusionModel
